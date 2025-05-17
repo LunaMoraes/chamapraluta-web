c5f85f4343cdd79b238e9142434bfdc3ca47c746
Luna <23488462+LunaMoraes@users.noreply.github.com>
2025-05-17 17:21:36 -0300

    created components and added tsparticles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router'; // Import Router
 import { NgIf } from '@angular/common';
+import { tsParticles } from "@tsparticles/engine";
+import { NgParticlesService, NgxParticlesModule } from "@tsparticles/angular";
+import { loadPolygonMaskPlugin } from "@tsparticles/plugin-polygon-mask";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, NgIf, NgxParticlesModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,12 +16,31 @@ export class AppComponent {
   title = 'chamapraluta';
   isLoggedIn = false;
 
-  constructor(private router: Router) {} // Inject Router
+  constructor(private router: Router, private readonly particlesService: NgParticlesService) {} // Inject Router
+  id = "tsparticles";
 
   navigateToLogin(): void {
     this.router.navigate(['/login']); 
   }
   navigateToDashboard(): void {
     this.router.navigate(['/']); 
+  }
+  navigateToCalendario(): void {
+    this.router.navigate(['/calendario']); 
+  }
+
+  async ngOnInit(): Promise<void> {
+    await this.particlesService.init(async () => {});
+    await loadPolygonMaskPlugin(tsParticles);
+    
+    await tsParticles.load({
+      id: this.id,
+      options: {
+        preset: 'fire',
+        background: {
+          color: '#000000'
+        }
+      }
+    });
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'dashboard', component: DashboardComponent }
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'calendario', component: CalendarioComponent }
 ];
