d8e65e41955281d315797f0f17f5398f7766a432
Luna <23488462+LunaMoraes@users.noreply.github.com>
2025-05-17 15:34:36 -0300

    basic website structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router'; // Import Router
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chamapraluta';
+  isLoggedIn = false;
+
+  constructor(private router: Router) {} // Inject Router
+
+  navigateToLogin(): void {
+    this.router.navigate(['/login']); 
+  }
+  navigateToDashboard(): void {
+    this.router.navigate(['/']); 
+  }
 }
