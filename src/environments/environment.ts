8f0ce10e550f6164c511e9cf0e710e7ad62d91fc
Luna <23488462+LunaMoraes@users.noreply.github.com>
2025-05-20 12:32:44 -0300

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  googleClientId: '892639456884-i244oceh154jmnvrjd4qg7vv84gs7rnv.apps.googleusercontent.com', // TODO: replace with your actual client ID
+  apiUrl: 'http://localhost:3000'
+};
