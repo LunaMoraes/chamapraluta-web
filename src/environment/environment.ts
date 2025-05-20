338202c6563e452b0dfe626b1d39d083cbc93a0e
Luna <23488462+LunaMoraes@users.noreply.github.com>
2025-05-20 12:34:45 -0300

    bug environment fix

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,4 @@
+// Added Google OAuth2 config and API URL
 export const environment = {
   production: false,
   googleClientId: '892639456884-i244oceh154jmnvrjd4qg7vv84gs7rnv.apps.googleusercontent.com', // TODO: replace with your actual client ID
