86fb71a45755b4764496cc3501604f2b99abc6d2
Luna <23488462+LunaMoraes@users.noreply.github.com>
2025-05-22 12:53:33 -0300

    cors Fix

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   googleClientId: '892639456884-i244oceh154jmnvrjd4qg7vv84gs7rnv.apps.googleusercontent.com', // TODO: replace with your actual client ID
-  apiUrl: 'http://localhost:8000/api/'
+  apiUrl: '/api'  // use relative path so proxy.conf.json can intercept
 };
